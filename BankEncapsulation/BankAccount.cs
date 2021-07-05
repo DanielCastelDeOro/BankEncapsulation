56d6f9b1f39d2d51cebcbbf9b1ec44eb6d71817f
Daniel Castel De Oro <83740100+DanielCastelDeOro@users.noreply.github.com>
2021-07-05 11:15:30 -0600

    Add project files.

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankEncapsulation
+{
+   public class BankAccount
+    {
+        private double _balance;
+
+            public void Deposit(double amount)
+        {
+            Console.WriteLine($"Simulating a depost of {amount, 0:c} to your account");
+            _balance = amount;
+        }
+
+        public double GetBalance()
+        {
+            return _balance;
+        }
+
+        
+    }
+}
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount bA = new BankAccount();
+
+            Console.WriteLine($"How much money do you want to deposit?");
+            double amountToDepost = double.Parse(Console.ReadLine());
+
+            bA.Deposit(amountToDepost);
+            double userBalance = bA.GetBalance();
+
+            Console.WriteLine($"Your balance is {userBalance, 0:c}");
+        }
+    }
+}
